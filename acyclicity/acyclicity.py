8e83e6beaf558ed6e85769f9243ac630f6054a52
Milana Stojadinov <mstojadinov@renderedtext.com>
2016-06-20 03:47:01 +0200

    Acyclicity

--- acyclicity/acyclicity.py
+++ acyclicity/acyclicity.py
@@ -4,7 +4,21 @@ import sys
 
 
 def acyclic(adj):
-    return 0
+    while sink_exists(adj):
+        remove(sink_exists(adj))
+    return 0 if len(adj) == 0 else 1
+
+def sink_exists(adj):
+    for node in adj:
+        if not node:
+            return node
+    return false
+
+def remove(node):
+    for vertex in adj:
+        if node in vertex:
+             vertex.remove(node)
+    adj.remove(node)
 
 if __name__ == '__main__':
     input = sys.stdin.read()
